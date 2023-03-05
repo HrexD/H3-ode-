1e45c8bd1f7445398560f1a4f800f80caae8ae75
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-03-05 19:57:00 +0100

    add password

--- init-mongo.js
+++ init-mongo.js
@@ -17,6 +17,7 @@ db.users.insertOne({
     "email": "admin@example.com",
     "password": {
         "salt": '19f7e5a4764f',
+        // password = admin
         "password": 'df09bc7a6be1d13a7858f0771f66bcd54aa5f95c5b0eeb264f34a812dde70d9764976878821f8ce62d95b79da9555acc115d8eaa31482b78ca70ce464e6e6806'
     },
     "role": "admin",
